0f2b28e398368492e4f0e402cbe84bc283b07844
alexriviello <alexriviello@gmail.com>
2024-12-31 11:49:49 -0500

    Updated player.py to allow movement, added constants, updated game loop

--- main.py
+++ main.py
@@ -2,29 +2,35 @@
 import pygame
 # imports all the code from the constants.py file
 from constants import *
+from player import *
 
 def main():
     # initialize pygame
     pygame.init()
     clock = pygame.time.Clock()
-    dt = clock.tick(60)/1000
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    while True:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    while True:
         # checks if user has closed the window, will exit if so
         for event in pygame.event.get():
           if event.type == pygame.QUIT:
              return
         # fill the screen
-        pygame.Surface.fill(screen, "black")
-        pygame.display.flip()
-        clock.tick(60)
+        screen.fill("black")
 
-    
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        for thing in updatable:
+            thing.update(dt)
+            thing.draw(screen)
+ 
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
